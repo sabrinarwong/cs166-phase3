3c9a0767e81171d2722708a8a055914564225615
Sabrina Wong <swong040@ucr.edu>
2018-12-13 03:17:06 -0800

    updated triggers.sql

---
+++ code/sql/triggers.sql
@@ -0,0 +1,5 @@
+CREATE SEQUENCE customer_id_seq OWNED BY Customer.id; 
+CREATE SEQUENCE mechanic_id_seq OWNED BY Mechanic.id;
+CREATE SEQUENCE owns_id_seq OWNED BY Owns.ownership_id;
+CREATE SEQUENCE rid_seq OWNED BY Service_Request.rid;
+CREATE SEQUENCE wid_seq OWNED BY Closed_Request.wid;
